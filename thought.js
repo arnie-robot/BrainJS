f36d8bf9a9d039ea0d71db9a489676b9764bc9e5
Chris <chris@chris-alexander.co.uk>
2011-02-08 16:44:27 +0000

    wait reset so zero goes to the next session got the brain reasoning and working, testing the environment correctly

--- thought.js
+++ thought.js
@@ -51,28 +51,110 @@ this.stop = function () {
 // executes the next thought tick
 this.next = function (obj) {
     // compute which trajectories are telling us to do what
-    result = { "x": -1, "y": -1, "z": -1 }
+    result = [-1, -1, -1];
     coordinates = ['x', 'y', 'z'];
 
     // loop over each trajectory we have to use
-    for (var trajectory in obj.trajectories) {
+    for (var trajectory = 0; trajectory < obj.trajectories.length; trajectory++) {
         // compute what step of the trajectory we are at
         var step = tick - obj.trajectories[trajectory][1];
         if (step >= obj.instructions.actions[obj.trajectories[trajectory][0]].trajectory.length) {
             // we have finished the trajectory - so remove it
             obj.trajectories.splice(trajectory, 1);
+            trajectory -= 1;
+            continue;
         } else if (step >= 0) {
             // loop over x, y, z coordinates
             for (var coord in coordinates) {
                 // we have not finished the trajectory and we have a valid start point
                 var coord_val = obj.instructions.actions[obj.trajectories[trajectory][0]].trajectory[step][coordinates[coord]];
                 if (coord_val >= 0) {
-                    if (result[coordinates[coord]] < 0) {
-                        result[coordinates[coord]] = coord_val;
+                    if (result[coord] < 0) {
+                        result[coord] = coord_val;
                     } else {
-                        result[coordinates[coord]] += coord_val;
+                        result[coord] += coord_val;
                     }
                 }
+            }
+        }
+
+        // see if we want to take the decision now
+        if (step == obj.instructions.actions[obj.trajectories[trajectory][0]].decision.executeAfter) {
+            // we are taking the decision
+            log('Executing decision');
+
+            try {
+                // verify arm state
+                armstate = obj.getLastPos();
+                expectedarm = obj.instructions.actions[obj.trajectories[trajectory][0]].decision.state.arm;
+                if (expectedarm.x != -1 && armstate[0] < (expectedarm.x[0] - expectedarm.x[1])) {
+                    throw "Arm X too low";
+                }
+                if (expectedarm.x != -1 && armstate[0] > (expectedarm.x[0] + expectedarm.x[1])) {
+                    throw "Arm X too high";
+                }
+                if (expectedarm.y != -1 && armstate[1] < (expectedarm.y[0] - expectedarm.y[1])) {
+                    throw "Arm Y too low";
+                }
+                if (expectedarm.y != -1 && armstate[1] > (expectedarm.y[0] + expectedarm.y[1])) {
+                    throw "Arm Y too high";
+                }
+                if (expectedarm.z != -1 && armstate[2] < (expectedarm.z[0] - expectedarm.z[1])) {
+                    throw "Arm Z too low";
+                }
+                if (expectedarm.z != -1 && armstate[2] > (expectedarm.z[0] + expectedarm.z[1])) {
+                    throw "Arm Z too high";
+                }
+
+                // verify object state
+                objects = obj.getLastObservation();
+                expectedobjects = obj.instructions.actions[obj.trajectories[trajectory][0]].decision.state.objects;
+                for (var object in expectedobjects) {
+                    var o = expectedobjects[object];
+                    for (var actual in objects) {
+                        var a = objects[actual];
+                        var matched = true;
+                        console.log(a);
+                        console.log(o);
+                        if (o.x != -1 && a[0] < (o.x[0] - o.x[1])) {
+                            matched = false;
+                        }
+                        if (o.x != -1 && a[0] > (o.x[0] + o.x[1])) {
+                            matched = false;
+                        }
+                        if (o.y != -1 && a[1] < (o.y[0] - o.y[1])) {
+                            matched = false;
+                        }
+                        if (o.y != -1 && a[1] > (o.y[0] + o.y[1])) {
+                            matched = false;
+                        }
+                        if (o.z != -1 && a[2] < (o.z[0] - o.z[1])) {
+                            matched = false;
+                        }
+                        if (o.z != -1 && a[2] > (o.z[0] + o.z[1])) {
+                            matched = false;
+                        }
+
+                        // we matched so no need to continue
+                        if (matched) {
+                            // splice out this result so it can't match another one
+                            objects.splice(actual, 1);
+                            break;
+                        }
+                    }
+                    // nothing matched so throw out
+                    if (!matched) {
+                        throw "Failed to find a match for object " + object;
+                    }
+                }
+
+                // we passed the tests
+                log("Decision conditions passed");
+                obj.addTrajectory(obj.instructions.actions[obj.trajectories[trajectory][0]].decision.success.action, obj.instructions.actions[obj.trajectories[trajectory][0]].decision.success.delay);
+            } catch (e) {
+                // we failed a test
+                log("Decision condition failed - " + e);
+                obj.addTrajectory(obj.instructions.actions[obj.trajectories[trajectory][0]].decision.fail.action, obj.instructions.actions[obj.trajectories[trajectory][0]].decision.fail.delay);
             }
         }
     }
@@ -86,9 +168,9 @@ this.next = function (obj) {
 // adds a trajectory to the execution stack
 this.addTrajectory = function (name, wait) {
     if (!wait) {
-        wait = 1;
+        wait = 0;
     }
-    this.trajectories.push([name, tick + wait]);
+    this.trajectories.push([name, tick + wait + 1]);
 }
 
 /**
